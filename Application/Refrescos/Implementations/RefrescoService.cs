a75fc68b3abff8643f2eec865a28a0081a9a97ea
Jeremy Vargas Artavia <jeremychaman@gmail.com>
2022-07-25 19:56:54 -0600

    Unit testing en los métodos de aplicación.

--- Application/Refrescos/Implementations/RefrescoService.cs
+++ Application/Refrescos/Implementations/RefrescoService.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace Application.Refrescos.Implementations
 {
-    internal class RefrescoService : IRefrescoService
+    public class RefrescoService : IRefrescoService
     {
         private readonly IRefrescoRepository _refrescoRepository;
 
